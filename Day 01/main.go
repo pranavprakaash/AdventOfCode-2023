6739fe4bb5f7f90661d3de7d751957f02aec0555
Pranav Prakash <hi.pranavprakash@gmail.com>
2023-12-06 01:44:40 +0530

    Day 1, complete.

--- Day 01/main.go
+++ Day 01/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // readInput reads the file and returns a slice of strings.
@@ -58,6 +60,7 @@ func calculateCalibrationValue(inputString string) int {
 		}
 	}
 
+	// Creating a new slice with only the first and last digits.
 	calibrationDigitsSlice := []string{calibrationDigitsSliceAll[0], calibrationDigitsSliceAll[len(calibrationDigitsSliceAll)-1]}
 
 	var calibrationDigitsString string
@@ -76,7 +79,87 @@ func calculateCalibrationValue(inputString string) int {
 
 }
 
+type CalibrationDigitsWithIndex struct {
+	value string
+	index int
+}
+
+func calculateUpdatedCalibrationValue(inputString string) int {
+	var calibrationDigitsAll []CalibrationDigitsWithIndex
+	crossCheckList := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+
+	for _, val := range crossCheckList {
+		if strings.Contains(inputString, val) {
+			firstIndex := strings.Index(inputString, val)
+			calibrationDigitsAll = append(calibrationDigitsAll, CalibrationDigitsWithIndex{value: val, index: firstIndex})
+
+			lastIndex := strings.LastIndex(inputString, val)
+			calibrationDigitsAll = append(calibrationDigitsAll, CalibrationDigitsWithIndex{value: val, index: lastIndex})
+
+		}
+	}
+
+	sort.Slice(calibrationDigitsAll[:], func(i, j int) bool { return calibrationDigitsAll[i].index < calibrationDigitsAll[j].index })
+
+	// Creating a new slice with only the first and last digits.
+	calibrationDigits := []string{calibrationDigitsAll[0].value, calibrationDigitsAll[len(calibrationDigitsAll)-1].value}
+
+	var calibrationDigitsFinal []string
+
+	for _, val := range calibrationDigits {
+		calibrationDigitsFinal = append(calibrationDigitsFinal, switchStrings(val))
+	}
+
+	var calibrationDigitsString string
+
+	for _, val := range calibrationDigitsFinal {
+		calibrationDigitsString += val
+	}
+
+	calibrationDigitsInt, err := strconv.Atoi(calibrationDigitsString)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return calibrationDigitsInt
+}
+
+func switchStrings(inputDigit string) string {
+	switch inputDigit {
+	case "one":
+		return "1"
+	case "two":
+		return "2"
+	case "three":
+		return "3"
+	case "four":
+		return "4"
+	case "five":
+		return "5"
+	case "six":
+		return "6"
+	case "seven":
+		return "7"
+	case "eight":
+		return "8"
+	case "nine":
+		return "9"
+	default:
+		return inputDigit
+	}
+}
+
+// To measure time taken.
+func timer(name string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", name, time.Since(start))
+	}
+}
+
 func main() {
+	defer timer("main")() // <-- The trailing () is the deferred call
 	inputSlice := readInput("input.txt")
 
 	var sum int = 0
@@ -86,4 +169,12 @@ func main() {
 	}
 
 	fmt.Println("The sum of calibration values is: ", sum)
+
+	sum = 0
+
+	for _, val := range inputSlice {
+		sum += calculateUpdatedCalibrationValue(val)
+	}
+
+	fmt.Println("The sum of calibration values using the updated function is: ", sum)
 }
